773683a270319e9100967472fd21afc88eed725a
Eagle2.0 <95177924+rit2001@users.noreply.github.com>
2026-04-14 22:40:33 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://smart-interview-agent2.onrender.com",
     credentials:true
 }))
 
