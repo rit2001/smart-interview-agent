7ba6d5ed81c4ff0480d2466c1def610372283a4f
Eagle2.0 <95177924+rit2001@users.noreply.github.com>
2026-04-14 22:37:38 +0530

    Update ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/interviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://smart-interview-agent1.onrender.com"
 
 function App() {
 
