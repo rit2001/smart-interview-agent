c1a20a6494077e880809fab7143be72fd650e7d2
Eagle2.0 <95177924+rit2001@users.noreply.github.com>
2026-04-23 22:32:00 +0530

    Change cookie secure flag to false

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,7 +25,7 @@ export const googleAuth = async (req,res) => {
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:true,
+            secure:false,
             sameSite:"none",
             maxAge:7*24*60*60*1000
 
